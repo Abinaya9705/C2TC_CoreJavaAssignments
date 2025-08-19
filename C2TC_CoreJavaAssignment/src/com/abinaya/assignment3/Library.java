e76c3ee2d8a16697906cd885e9b1a52dd5b74df6
Abinaya <abiss9705@gmail.com>
2025-08-19 19:51:17 +0530

    I have showcased my assignment 3 on inheritance.

---
+++ C2TC_CoreJavaAssignment/src/com/abinaya/assignment3/Library.java
@@ -0,0 +1,31 @@
+package com.abinaya.assignment3;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.abinaya.assignment3.Item;
+
+public class Library {
+    private List<Item> items;
+
+    public Library() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void showAllItems() {
+        System.out.println("Library contains:");
+        for (Item item : items) {
+            if (item instanceof com.abinaya.assignment3.Book book) {
+                System.out.println("- " + book.getTitle() + " by " + book.getAuthor());
+            } else if (item instanceof com.abinaya.assignment3.Magzine magzine) {
+                System.out.println("- " + magzine.getTitle() + " Issue " + magzine.getIssueNumber());
+            } else {
+                System.out.println("- " + item.getTitle());
+            }
+        }
+    }
+}
+
